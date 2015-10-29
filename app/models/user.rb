dd44142c820d7b29d56426764b408b99e18433df
Yoshinori Kawasaki <ykawasaki@gmail.com>
2015-10-29 14:27:49 +0900

    Make it slow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
   has_many :relations
 
-  def friend_ids
-    relations.map(&:friend_id)
-  end
-
-  def mutual_friend_ids(friend)
-     friend_ids & friend.friend_ids
+  def slow_mutual_friend_ids(friend)
+    Relation.joins("JOIN relations t2 on relations.friend_id = t2.user_id")
+      .where("relations.user_id = ? AND t2.friend_id = ?", id, friend.id)
+      .select("relations.friend_id")
+      .map(&:friend_id)
+      .uniq
   end
 
   def mutual_friends(friend)
-    User.where(id: mutual_friend_ids(friend))
+    User.where(id: slow_mutual_friend_ids(friend))
   end
 end
