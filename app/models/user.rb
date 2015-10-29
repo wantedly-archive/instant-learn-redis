c894c968a00994c154440d4545386497c5fee2f5
Yoshinori Kawasaki <ykawasaki@gmail.com>
2015-10-29 14:10:42 +0900

    Implement User#mutual_friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   has_many :relations
+
+  def friend_ids
+    relations.map(&:friend_id)
+  end
+
+  def mutual_friend_ids(friend)
+     friend_ids & friend.friend_ids
+  end
+
+  def mutual_friends(friend)
+    User.where(id: mutual_friend_ids(friend))
+  end
 end
