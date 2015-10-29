91f23d59ca8b38f95524dfb13a1ac059b7d8391d
Yoshinori Kawasaki <ykawasaki@gmail.com>
2015-10-29 14:04:44 +0900

    Use AR.import to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,25 @@
 
 require 'faker'
 
+users = []
 1000.times do
-  User.create!(name: Faker::Name.name, email: Faker::Internet.email, avatar_url: Faker::Avatar.image)
+  users << User.new(name: Faker::Name.name, email: Faker::Internet.email, avatar_url: Faker::Avatar.image)
 end
+User.import(users)
 
-first_user_id = USer.first.id
+first_user_id = User.first.id
 last_user_id = User.last.id
-user_id_range = first_user_id..last_user_id
-user_ids = user_id_range.to_a
+user_ids = (first_user_id..last_user_id).to_a
 
-user_id_range.each do |user_id|
-  user_ids.sample(500) do |friend_id|
-    if user_id != friend_id
-      Relation.create!(user_id: user_id, friend_id: friend_id)
-      Relation.create!(user_id: friend_id, friend_id: user_id)
+user_ids.each_slice(100) do |sliced|
+  relations = []
+  sliced.each do |user_id|
+    user_ids.sample(100).each do |friend_id|
+      if user_id != friend_id
+        relations << Relation.new(user_id: user_id, friend_id: friend_id)
+        relations << Relation.new(user_id: friend_id, friend_id: user_id)
+      end
     end
   end
+  Relation.import(relations)
 end
